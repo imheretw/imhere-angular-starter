714fb76adc2b6d80326b470b50971301528bf0b8
Kos <koshuang@gmail.com>
2017-03-12 01:54:53 +0800

    refactor(component): [sidebarAvatar] extract

--- src/app/admin/components/adminSidebar.js
+++ src/app/admin/components/adminSidebar.js
@@ -4,6 +4,7 @@ import 'common/core';
 import ReduxController from 'common/controllers/ReduxController';
 import isActiveItem from 'common/directives/isActiveItem';
 import sidebarNavMenuModule from './sidebarNavMenu';
+import sidebarAvatarModule from './sidebarAvatar';
 
 class AdminSidebarController extends ReduxController {
   /*@ngInject*/
@@ -73,19 +74,8 @@ const adminSidebar = {
         <h1 ng-show="!vm.adminLayout.sideBarOpened">
           <i class="fa fa-github" aria-hidden="true"></i>
         </h1>
-
       </div>
-      <div class="media admin-sidebar__user" ng-show="vm.adminLayout.sideBarOpened">
-        <div class="media-left ">
-          <a ui-sref="admin.profile">
-            <div class="admin-sidebar__user--img" style="background-image: url({{vm.currentUser.img}});"></div>
-          </a>
-        </div>
-        <div class="media-body admin-sidebar__user--body">
-          <p>Welcome!</p>
-          <a ui-sref="admin.profile"><h5 class="media-heading">{{vm.currentUser.name}}</h5></a>
-        </div>
-      </div>
+      <sidebar-avatar user="vm.currentUser" full-mode="vm.adminLayout.sideBarOpened"></sidebar-avatar>
       <sidebar-nav-menu items="vm.sideBarNav" full-mode="vm.adminLayout.sideBarOpened"></sidebar-nav-menu>
       <div class="admin-sidebar__footer" ng-class="{false:'sm'}[vm.adminLayout.sideBarOpened]">
         <a ng-click="vm.logout()" ng-show="vm.adminLayout.sideBarOpened">Log Out  <i class="fa fa-sign-out" aria-hidden="true"></i></a>
@@ -98,6 +88,7 @@ const adminSidebar = {
 export default angular
   .module('common.components.panels.admin.sidebar', [
     sidebarNavMenuModule.name,
+    sidebarAvatarModule.name,
     isActiveItem.name,
   ])
   .component('adminSidebar', adminSidebar);
